3a5de6b5583dcd6aaa33f69223d661efcb5f166d
ryantkm <ryantkm@gmail.com>
2017-12-18 22:46:10 +0800

    1st Commit: FirebaseUI Auth

---
+++ app/src/main/java/com/eventdee/travyplan/MainActivity.java
@@ -0,0 +1,182 @@
+package com.eventdee.travyplan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.eventdee.travyplan.utils.Constants;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == Constants.SIGN_IN_REQUEST) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Welcome " + firebaseUser.getDisplayName() + "!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in cancelled! " + response.getErrorCode(), Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "Sign in cancelled!", Toast.LENGTH_SHORT).show();
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No internet connection!", Toast.LENGTH_SHORT).show();
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, "Unknown error!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Unknown status!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        //Now check if this user is null
+        if (firebaseUser == null){
+            //send user to the login page
+            // Create and launch sign-in intent
+            signInPage();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            signInPage();
+                        }
+                    });
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void signInPage() {
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+// TODO:                       .setTheme(R.style.LoginTheme)
+// TODO:                       .setLogo(R.drawable.travy_logo)
+                        .build(),
+                Constants.SIGN_IN_REQUEST);
+    }
+}
---
+++ app/src/main/java/com/eventdee/travyplan/utils/Constants.java
@@ -0,0 +1,101 @@
+package com.eventdee.travyplan.utils;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    // constants relating to request codes
+    public static final int TRIP_DETAILS_REQUEST = 1;
+    public static final int ADD_TRIP_REQUEST = 2;
+    public static final int ADD_TRIP_ITEM_REQUEST = 3;
+    public static final int GET_IMAGE_REQUEST = 4;
+    public static final int SIGN_IN_REQUEST = 5;
+
+//    public static final ArrayList<Integer> mIcons = new ArrayList<Integer>() {{
+//        add(R.drawable.ic_directions_bike);
+//        add(R.drawable.ic_widgets);
+//        add(R.drawable.ic_directions_run);
+//        add(R.drawable.ic_movie);
+//        add(R.drawable.ic_local_bar);
+//        add(R.drawable.ic_shopping_cart);
+//        add(R.drawable.ic_flag);
+//        add(R.drawable.ic_airplanemode_active);
+//        add(R.drawable.ic_work);
+//        add(R.drawable.ic_fitness_center);
+//        add(R.drawable.ic_local_see);
+//        add(R.drawable.ic_insert_photo);
+//        add(R.drawable.ic_audiotrack);
+//        add(R.drawable.ic_subscriptions);
+//        add(R.drawable.ic_book);
+//        add(R.drawable.ic_child_care);
+//        add(R.drawable.ic_child_friendly);
+//        add(R.drawable.ic_insert_invitation);
+//        add(R.drawable.ic_person);
+//        add(R.drawable.ic_group);
+//        add(R.drawable.ic_wc);
+//        add(R.drawable.ic_favorite);
+//        add(R.drawable.ic_star);
+//        add(R.drawable.ic_directions_walk);
+//        add(R.drawable.ic_pool);
+//        add(R.drawable.ic_rowing);
+//        add(R.drawable.ic_golf_course);
+//        add(R.drawable.ic_terrain);
+//        add(R.drawable.ic_directions_boat);
+//        add(R.drawable.ic_directions_bus);
+//        add(R.drawable.ic_directions_car);
+//        add(R.drawable.ic_directions_subway);
+//        add(R.drawable.ic_local_taxi);
+//        add(R.drawable.ic_local_shipping);
+//        add(R.drawable.ic_motorcycle);
+//        add(R.drawable.ic_map);
+//        add(R.drawable.ic_public);
+//        add(R.drawable.ic_my_location);
+//        add(R.drawable.ic_near_me);
+//        add(R.drawable.ic_place);
+//        add(R.drawable.ic_home);
+//        add(R.drawable.ic_hotel);
+//        add(R.drawable.ic_store);
+//        add(R.drawable.ic_location_city);
+//        add(R.drawable.ic_school);
+//        add(R.drawable.ic_beach_access);
+//        add(R.drawable.ic_casino);
+//        add(R.drawable.ic_spa);
+//        add(R.drawable.ic_restaurant);
+//        add(R.drawable.ic_local_cafe);
+//        add(R.drawable.ic_cake);
+//        add(R.drawable.ic_room_service);
+//        add(R.drawable.ic_shopping_basket);
+//        add(R.drawable.ic_local_florist);
+//        add(R.drawable.ic_pets);
+//        add(R.drawable.ic_notifications);
+//        add(R.drawable.ic_whatshot);
+//        add(R.drawable.ic_smoking_rooms);
+//        add(R.drawable.ic_phone_in_talk);
+//        add(R.drawable.ic_voice_chat);
+//        add(R.drawable.ic_sms);
+//        add(R.drawable.ic_insert_comment);
+//        add(R.drawable.ic_email);
+//        add(R.drawable.ic_headset_mic);
+//        add(R.drawable.ic_keyboard_voice);
+//        add(R.drawable.ic_wifi);
+//        add(R.drawable.ic_phone_android);
+//        add(R.drawable.ic_tv);
+//        add(R.drawable.ic_attachment);
+//        add(R.drawable.ic_attach_money);
+//        add(R.drawable.ic_alarm);
+//        add(R.drawable.ic_lightbulb_outline);
+//        add(R.drawable.ic_pan_tool);
+//        add(R.drawable.ic_watch_later);
+//        add(R.drawable.ic_format_paint);
+//        add(R.drawable.ic_weekend);
+//        add(R.drawable.ic_credit_card);
+//        add(R.drawable.ic_thumb_up);
+//        add(R.drawable.ic_thumb_down);
+//        add(R.drawable.ic_share);
+//        add(R.drawable.ic_search);
+//        add(R.drawable.ic_sync);
+//    }};
+
+
+}
